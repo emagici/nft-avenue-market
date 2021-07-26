d4de51cc12dcd2b75c821fee9b3bd2a14c15e1ba
VinceCrypto <vinceat2018@outlook.com>
2021-07-27 00:18:33 +0800

    update url for nodeapi

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -2,12 +2,15 @@ export default class AppUrls {
     static fomoHost = "http://fomobo.workinjupiter.club";
     static fomoHostApi = "https://fomoapi.workinjupiter.club";
     static fomoClient = "http://fomofa.attspace.com";
+    static fomoNodeAPI = "https://staging-locker.workinjupiter.club";
 
     // static fomoHost = "http://localhost:4200";
     // static fomoHostApi = "https://localhost:44301";
     // static fomoClient = "http://localhost:3000";
+    // static fomoNodeAPI = "https://staging-locker.workinjupiter.club/";
 
     // static fomoHost = "http://0.0.0.0:4200";
     // static fomoHostApi = "https://0.0.0.0:44301";
     // static fomoClient = "http://0.0.0.0:3001";
+    // static fomoNodeAPI = "https://staging-locker.workinjupiter.club/";
 };
