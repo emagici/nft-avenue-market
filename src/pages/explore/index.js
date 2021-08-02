dba61c93ec4ba00101200cef1d25b9b1ae40596d
emagici <skrullweb.dev@gmail.com>
2021-08-02 15:06:14 +0100

    added color anim to main banner

--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import TextBanner from '../../components/banner/text-banner'
 import VideoBanner from '../../components/banner/video-banner'
 import Sellers from './popular'
@@ -10,12 +10,37 @@ import Following from './following'
 
 
 export default function Explore() {
+  const [colorIndex, setColorIndex] = useState(0);
+  const [promoColors, setPromoColors] = useState({ bgColor: "bg-blue-700", textColor: "text-white" })
+  const colors = [
+    { bgColor: "bg-green-500", textColor: "text-black" },
+    { bgColor: "bg-red-500", textColor: "text-white" },
+    { bgColor: "bg-yellow-400", textColor: "text-black" },
+    { bgColor: "bg-indigo-600", textColor: "text-white" },
+  ]
+
+  useEffect(() => {
+    const changeBanner = setInterval(() => {
+      setColorIndex(prevState => (prevState+1) == colors.length ? 0 : prevState+1)
+    }, 1000)
+
+    return () => {
+      clearInterval(changeBanner)
+    }
+  }, [])
+
+  useEffect(() => {
+    setPromoColors(colors[colorIndex])
+  }, [colorIndex])
+
+
   return (
     <div className="max-w-screen-2xl mx-auto px-4 sm:px-6">
       <div className="mb-20">
         <TextBanner
           title="The New Creative Economy"
           subtitle="Create, Explore, Collect Digital Art NFTs"
+          {...promoColors}
         />
         {/* <VideoBanner /> */}
       </div>
