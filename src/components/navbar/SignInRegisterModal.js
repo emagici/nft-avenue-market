2ebd0fd03d63da161a288d1f89d3618caae425d6
Kugan <kugan@attspace.ventures>
2021-08-16 16:40:30 +0800

    Disabled sign-in/register with email

--- src/components/navbar/SignInRegisterModal.js
+++ src/components/navbar/SignInRegisterModal.js
@@ -57,13 +57,15 @@ export default function SignInRegisterModal(props) {
               <p className="text-sm text-gray-500 mb-5">Choose an option</p>
               <div className="md:flex justify-center items-center mb-3">
                 <button
-                  onClick={() => optionSelected('email')}
-                  className="flex md:flex-col justify-center items-center mb-3 px-4 py-2 w-full mx-1.5 md:w-32 md:h-40 shadow-lg text-sm font-bold rounded-2xl border border-gray-200 text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none"
+                  // onClick={() => optionSelected('email')}
+                  className="flex md:flex-col justify-center items-center mb-3 px-4 py-2 w-full mx-1.5 md:w-32 md:h-40 shadow-lg text-sm font-bold rounded-2xl border border-gray-200 text-gray-700 bg-gray-300 cursor-not-allowed focus:outline-none"
                 >
                   <div className="flex justify-center mb-1">
                     <MailIcon className="h-5 w-5 md:h-7 md:w-7 mr-1 md:mr-0 text-gray-700" aria-hidden="true" />
                   </div>
                   <p className="">Sign In With Email</p>
+                  <br />
+                  <p className="text-red-500"><i>Coming Soon</i></p>
                 </button>
                 <button
                   onClick={() => optionSelected('metamask')}
@@ -76,13 +78,15 @@ export default function SignInRegisterModal(props) {
                   <p>Sign In With MetaMask</p>
                 </button>
                 <button
-                  onClick={() => optionSelected('register')}
-                  className="flex md:flex-col justify-center items-center mb-3 px-4 py-2 w-full mx-1.5 md:w-32 md:h-40 shadow-lg text-sm font-bold rounded-2xl border border-gray-200 text-gray-700 bg-gray-100 hover:bg-gray-200 focus:outline-none"
+                  // onClick={() => optionSelected('register')}
+                  className="flex md:flex-col justify-center items-center mb-3 px-4 py-2 w-full mx-1.5 md:w-32 md:h-40 shadow-lg text-sm font-bold rounded-2xl border border-gray-200 text-gray-700 bg-gray-300 cursor-not-allowed focus:outline-none"
                 >
                   <div className="flex justify-center mb-1">
                     <PencilAltIcon className="h-5 w-5 md:h-7 md:w-7 mr-1 md:mr-0 text-gray-700" aria-hidden="true" />
                   </div>
                   <p>Register New Account</p>
+                  <br />
+                  <p className="text-red-500"><i>Coming Soon</i></p>
                 </button>
               </div>
             </div>
