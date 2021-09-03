c6d2b8c60eed687774624149e3c09d6a5c51c253
emagici <skrullweb.dev@gmail.com>
2021-09-04 00:20:31 +0100

    fixed verified z-index and staging2 notifcation links

--- src/AppSettings.js
+++ src/AppSettings.js
@@ -1,7 +1,7 @@
 export default class AppUrls {
     static fomoHost = "http://fomobo.workinjupiter.club";
     static fomoHostApi = "https://api.theavenue.market";
-    static fomoClient = "https://staging2.theavenue.market";
+    static fomoClient = "https://theavenue.market";
     static fomoNodeAPI = "";
 
     // static fomoHost = "http://localhost:4200";
