cb42274c33960eecbee24b4e456ee46be6da46b4
emagici <skrullweb.dev@gmail.com>
2021-09-03 23:43:57 +0100

    various bugfixes and improvements

--- src/pages/explore/index.js
+++ src/pages/explore/index.js
@@ -52,7 +52,7 @@ export default function Explore() {
       <Following/> */}
       <HottestBidsSection/>
       <RecentlyAddedSection/>
-      <GettingStartedSection/>
+      {/* <GettingStartedSection/> */}
     </div>
   )
 }
